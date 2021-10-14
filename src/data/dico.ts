9a90dc6ea68ebb727eff378759ae54612c81827c
Willy Brauner <mail@willybrauner.com>
2021-10-14 22:02:07 +0200

    Update dico

--- src/data/dico.ts
+++ src/data/dico.ts
@@ -6,7 +6,7 @@ export const DICO = {
   github_link: "https://github.com/willybrauner/blurry-faces",
 
   credits: "design & dev by Willy B.",
-  donate: "it’s usefull? pay me a 🍺",
+  donate: "It’s useful? Pay me a 🍺",
   donate_link:
     "https://www.paypal.com/donate/?business=48CH6BLTZSRPA&no_recurring=0&currency_code=EUR",
   author_webiste: "https://willybrauner.com",
